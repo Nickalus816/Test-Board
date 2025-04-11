35b4999af4596df03c21484b684c485294ecf060
Nickalus816 <lowreynickalus2@gmail.com>
2025-04-11 14:43:55 -0400

    Current TestBoard

--- src/main/java/frc/robot/subsystems/TestBoard.java
+++ src/main/java/frc/robot/subsystems/TestBoard.java
@@ -1,22 +1,37 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.ADIS16448_IMU;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TestBoard extends SubsystemBase{
     private final SparkMax m_testOne;
     private final SparkMax m_testTwo;
     private final SparkMaxConfig m_testOneConfig;
     private final SparkMaxConfig m_testTwConfig;
+
     private final DigitalInput m_switch;
     private final DigitalInput m_sensor;
+
+    private final ADIS16448_IMU m_Gyro;
+    private boolean isFlipped;
+    private boolean useHeadingCorrection;
+    private Rotation2d correctHeadingTargetHeading;
+    private Timer correctHeadingTimer;
+    private double correctHeadingPreviousTime;
+    private double correctHeadingOffTime;
+    private double heading;
+
+
     public TestBoard(){
         m_testOne = new SparkMax(10, MotorType.kBrushed);
         m_testTwo = new SparkMax(11, MotorType.kBrushed);
@@ -37,6 +52,31 @@ public class TestBoard extends SubsystemBase{
 
         m_switch = new DigitalInput(0);
         m_sensor = new DigitalInput(1);
+
+        m_Gyro = new ADIS16448_IMU();
+        isFlipped = false;
+        useHeadingCorrection = true;
+        correctHeadingTimer = new Timer();
+        correctHeadingTimer.start();
+        correctHeadingPreviousTime = 0.0;
+        correctHeadingOffTime = 0.0;
+        correctHeadingTargetHeading = getHeadingAsRotation2d();
+
+
+    }
+
+
+    
+    public double getHeading(){
+        heading = m_Gyro.getGyroAngleZ();
+        //m_Gyro.getAngle(m_Gyro.getYawAxis()); ADIS16470 Version
+        return Math.IEEEremainder(heading, 360);
+    }
+
+    public Rotation2d getHeadingAsRotation2d(){
+        heading = getHeading();
+        return Rotation2d.fromDegrees(heading);
+
     }
 
     public void drive(){
@@ -62,6 +102,7 @@ public class TestBoard extends SubsystemBase{
     @Override 
     public void periodic(){
         SmartDashboard.putBoolean("BBrake", m_sensor.get());
+        SmartDashboard.putNumber("gyro", m_Gyro.getGyroAngleZ());
     }
 
 }
